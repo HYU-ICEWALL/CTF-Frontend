e9e3c288ce6104a3b04e36ab2bf5f38441838490
ingyu1008 <ingyu1008@gmail.com>
2024-05-24 11:19:54 +0900

    change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,15 @@ import "../styles/Footer.css";
 function Footer() {
   return (
     <>
-      <footer>
+      <footer style={
+          {
+            fontSize: '10px',
+            color: 'gray'
+          }
+        }>
         <p>ICEWALL © 2024</p>
+        <p>Frontend design and development by @ingyu_1008</p>
+        <p>Backend development by @joo_mj24</p>
       </footer>
     </>
   );
