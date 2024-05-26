34835417ab186b6edd3274ca58273a6d33d9d52d
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-05-26 22:47:31 +0900

    fix fetch issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,33 +4,39 @@ import { useState } from "react";
 
 function Header() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [fetched, setFetched] = useState(false);
 
   if(loggedIn == false && document.cookie.includes("loggedin=true")) {
     setLoggedIn(true);
   }
 
-  fetch("https://server.icewall.org/api/account/auth", {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true", // Add this line
-    },
-    credentials: "include",
-  }).then((res) => {
-    res.json().then((data) => {
-      console.log(data);
-      if (data["code"] === 0) {
-        setLoggedIn(true);
-      } else {
-        setLoggedIn(false);
-        document.cookie = "loggedin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-        document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-      }
+  if(fetched == false) {
+    setFetched(true);
+    fetch("https://server.icewall.org/api/account/auth", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true", // Add this line
+      },
+      credentials: "include",
+    }).then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+        if (data["code"] === 0) {
+          setLoggedIn(true);
+        } else {
+          setLoggedIn(false);
+          document.cookie = "loggedin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+          document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+        }
+      });
     });
-  });
+  }
+
+  
 
   return (
     <>
