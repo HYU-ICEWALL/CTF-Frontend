162581115448e39342bf048cc38bc721ca846185
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-05-26 23:05:38 +0900

    update Logout.tsx

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,7 +1,7 @@
 function Logout() {
   // remove sid cookie
 
-  fetch("https://server.icewall.org/api/account/logout", {}).then((res) => {
+  fetch("/api/account/logout", {}).then((res) => {
     document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "loggedin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
