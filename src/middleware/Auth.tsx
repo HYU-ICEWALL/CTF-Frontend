575fb2a1bf083da461c593cafe51233bc763a245
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-05-26 23:07:48 +0900

    update auth module

--- src/middleware/Auth.tsx
+++ src/middleware/Auth.tsx
@@ -6,30 +6,34 @@ interface AuthProps {
 
 function Auth(props: AuthProps) {
   const [isAuth, setIsAuth] = useState(false);
+  const [fetched, setFetched] = useState(false);
 
-  fetch("/api/account/auth", {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": "true", // Add this line
-    },
-    credentials: "include",
-  }).then((res) => {
-    res.json().then((data) => {
-      if (data["code"] === 0) {
-        setIsAuth(true);
-        document.cookie = "loggedin=true; path=/;"
-      } else {
-        setIsAuth(false);
-        document.cookie = "loggedin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-        document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-        window.location.href = "/login";
-      }
+  if(fetched == false) {
+    setFetched(true);
+    fetch("/api/account/auth", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true", // Add this line
+      },
+      credentials: "include",
+    }).then((res) => {
+      res.json().then((data) => {
+        if (data["code"] === 0) {
+          setIsAuth(true);
+          document.cookie = "loggedin=true; path=/;"
+        } else {
+          setIsAuth(false);
+          document.cookie = "loggedin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+          document.cookie = "connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+          window.location.href = "/login";
+        }
+      });
     });
-  });
+  }  
   
   return (
     <>
