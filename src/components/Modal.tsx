51656cbfad39d43241c0fe53b4e1a2f44f90e5dd
ingyu1008 <ingyu1008@gmail.com>
2024-05-28 18:27:05 +0900

    add submission butotn

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,6 +4,8 @@ interface ModalProps {
     children: React.ReactNode;
     hidden: any;
     setHidden: any;
+    submitLink: any;
+    probName: any;
 }
 
 function Modal(props : ModalProps) {
@@ -11,23 +13,58 @@ function Modal(props : ModalProps) {
         props.setHidden(true);
     }
 
-  return (
-    <>
-    <div className="wrapper" hidden={props.hidden}>
-        <div className="modal">
-            {props.children}
+const submitHandler = (e : any) => {
+    e.preventDefault();
 
-            <div className="buttonwrapper">
-                <form>
-                    <input type="text" placeholder="답안을 입력하세요"></input>
-                    <button>제출</button>
-                </form>
-                <button onClick={clickHandler} >닫기</button>
+    const form = e.target;
+    const data = new FormData(form);
+    const answer = data.get("answer");
+
+    fetch(props.submitLink, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true", // Add this line
+        },
+        credentials: "include",
+        body: JSON.stringify({
+            name: props.probName,
+            flag: answer,
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.result === "correct") {
+            alert("정답입니다!");
+            props.setHidden(true);
+        } else {
+            alert("틀렸습니다!");
+        }
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+};
+
+return (
+    <>
+        <div className="wrapper" hidden={props.hidden}>
+            <div className="modal">
+                {props.children}
+
+                <div className="buttonwrapper">
+                    <form onSubmit={submitHandler}>
+                        <input type="text" name="answer" placeholder="답안을 입력하세요" />
+                        <button type="submit">제출</button>
+                    </form>
+                    <button onClick={clickHandler}>닫기</button>
+                </div>
             </div>
         </div>
-    </div>
     </>
-  );
+);
 }
 
 export default Modal;
