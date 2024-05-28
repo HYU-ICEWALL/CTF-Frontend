f9a52aac49bfc38f7b0b0ae482dc36c440760138
ingyu1008 <ingyu1008@gmail.com>
2024-05-28 16:48:05 +0900

    commit scoreboard prototype with dummy data

--- src/pages/Scoreboard.tsx
+++ src/pages/Scoreboard.tsx
@@ -8,10 +8,39 @@ import { createTheme, useTheme, ThemeProvider } from '@mui/material/styles';
 function Scoreboard() {
   const [data, setData] = useState([]);
   const [fetched, setFetched] = useState(false);
-  const theme = useTheme();
 
   if(fetched === false){
     setFetched(true);
+
+    fetch("/api/contest/recent", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true", // Add this line
+      },
+      credentials: "include",
+    })
+    .then((res) => res.json())
+    .then((data) => data["data"]["recent"])
+    .then((recent) => {
+      fetch("/api/contest/scoreboard?name="+recent, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true", // Add this line
+        },
+        credentials: "include",
+      }).then((res) => res.json())
+      .then((data) => {
+        return data;
+      });
+    }).then((data) => {
+      console.log(data);
+    });
 
     fetch("/api/contest/scoreboard", {
       method: "GET",
@@ -24,47 +53,49 @@ function Scoreboard() {
       credentials: "include",
     }).then((res) => res.json())
     .then((data) => {
-
+      console.log(data);
     });
   }
 
   const newTheme = createTheme({ palette: { mode: "dark" } });
 
-  return <>
-          <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-          <div className="scoreboard">
-            <ThemeProvider theme={newTheme}>
-            <LineChart
-              xAxis={[{ data: [1, 2, 3, 5, 8, 10]}]}
-              series={[
-                {
-                  data: [1, 3, 5, 7, 9 , 11],
-                },
-                {
-                  data: [0, 0, 1, 1, 3, 6],
-                },
-                {
-                  data: [0, 2, 5, 8, 8, 10],
-                },
-              ]}
-            />
-            </ThemeProvider>
-          </div>
-        </>;
+  // return <>
+  //         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+  //         <div className="scoreboard">
+  //           <ThemeProvider theme={newTheme}>
+  //           <LineChart
+  //             xAxis={[{ data: [1, 2, 3, 5, 8, 10]}]}
+  //             series={[
+  //               {
+  //                 data: [1, 3, 5, 7, 9 , 11],
+  //               },
+  //               {
+  //                 data: [0, 0, 1, 1, 3, 6],
+  //               },
+  //               {
+  //                 data: [0, 2, 5, 8, 8, 10],
+  //               },
+  //             ]}
+  //           />
+  //           </ThemeProvider>
+  //         </div>
+  //       </>;
 
   return <Auth>{
     <>
-      <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
       <div className="scoreboard">
         <ThemeProvider theme={newTheme}>
           <LineChart
             xAxis={[{ data: [1, 2, 3, 5, 8, 10]}]}
             series={[
               {
-                data: [2, 5.5, 2, 8.5, 1.5, 5],
+                data: [1, 3, 5, 7, 9 , 11],
               },
               {
-                data: [3, 10, 2, 5, 2, 1],
+                data: [0, 0, 1, 1, 3, 6],
+              },
+              {
+                data: [0, 2, 5, 8, 8, 10],
               },
             ]}
           />
